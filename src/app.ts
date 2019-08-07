e7e9acedf2ab8ec08a3c840e09c5df77593d9996
Jackson <jacksonga@hotmail.com>
2019-08-07 02:45:26 -0300

    Finalizando o projeto

--- src/app.ts
+++ src/app.ts
@@ -2,37 +2,37 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
+import routes from "./routes";
+
 class App {
-    public express: express.Application
+  public express: express.Application
 
-    // Constructor
-    public constructor () {
-      this.express = express()
+  // Constructor
+  public constructor() {
+    this.express = express()
 
-      this.middlewares()
-      this.database()
-      this.routes()
-    }
+    this.middlewares()
+    this.database()
+    this.routes()
+  }
 
-    // Middlewares
-    private middlewares (): void {
-      this.express.use(express.json())
-      this.express.use(cors())
-    }
+  // Middlewares
+  private middlewares(): void {
+    this.express.use(express.json())
+    this.express.use(cors())
+  }
 
-    // Databases
-    private database (): void {
-      mongoose.connect('mongodb://localhost:27017/tsnode', {
-        useNewUrlParser: true
-      })
-    }
+  // Databases
+  private database(): void {
+    mongoose.connect('mongodb://localhost:27017/tsnode', {
+      useNewUrlParser: true
+    })
+  }
 
-    // Routes
-    private routes (): void {
-      this.express.get('/', (req, res) => {
-        return res.send('Hello World')
-      })
-    }
+  // Routes
+  private routes(): void {
+    this.express.use(routes)
+  }
 }
 
 export default new App().express
